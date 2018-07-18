be6f9d236d7438d769afe27dedf5a3be598228c0
Letticia Nicoli <letticia.nicoli@lambda3.com.br>
2018-07-18 10:49:35 -0300

    Init project

---
+++ demoMVC/Controllers/HomeController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using demoMVC.Models;
+
+namespace demoMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Sum()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Addition()
+        {
+            int number1 = Convert.ToInt32(HttpContext.Request.Form["txtFirst"].ToString());
+            int num2 = Convert.ToInt32(HttpContext.Request.Form["txtSecond"].ToString());
+
+            int result = number1 + num2;
+
+            ViewBag.SumResult = result.ToString();
+
+            return View("Sum");
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
